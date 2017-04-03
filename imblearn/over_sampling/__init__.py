572f22cfa1689ceeec50b248ef7d411bf04f1fa1
Christophe Rannou <christophe.rannou@corp.ovh.com>
2017-04-03 10:55:25 +0200

    forgot HEAD

--- imblearn/over_sampling/__init__.py
+++ imblearn/over_sampling/__init__.py
@@ -6,7 +6,6 @@ perform over-sampling.
 from .random_over_sampler import RandomOverSampler
 from .smote import SMOTE
 from .adasyn import ADASYN
-<<<<<<< HEAD
 from .smotenc import SMOTENC
 
 __all__ = ['RandomOverSampler', 'SMOTE', 'ADASYN', 'SMOTENC']
